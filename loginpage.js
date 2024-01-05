99baa031ce8a0c338a72dd9155e4e2dfb705a672
MikeD2802 <125842806+MikeD2802@users.noreply.github.com>
2024-01-05 01:11:40 -0800

    Update loginpage.js

--- loginpage.js
+++ loginpage.js
@@ -1,8 +1,1 @@
-function validateForm() {
-    var username = document.forms["registrationForm"]["username"].value;
-    var password = document.forms["registrationForm"]["password"].value;
-    if (username == "" || password == "") {
-        alert("Both username and password must be filled out");
-        return false;
-    }
-}
+
