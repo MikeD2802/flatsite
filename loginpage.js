616efdbc629f6d03bfd9dc609407baa1b39a89a7
MikeD2802 <125842806+MikeD2802@users.noreply.github.com>
2024-01-05 00:44:24 -0800

    Update loginpage.js

--- loginpage.js
+++ loginpage.js
@@ -1,10 +1,8 @@
-// Example JavaScript for form validation
 function validateForm() {
     var username = document.forms["registrationForm"]["username"].value;
-    if (username == "") {
-        alert("Username must be filled out");
+    var password = document.forms["registrationForm"]["password"].value;
+    if (username == "" || password == "") {
+        alert("Both username and password must be filled out");
         return false;
     }
-
-    // Add more validation rules as needed
 }
